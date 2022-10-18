be68e76a141ea12464fe8c0960153e719b2f8e63
coblu3 <demirel.akif@hotmail.com>
2022-10-19 02:30:06 +0300

    first commit

---
+++ contracts/SimpleStorage.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract SimpleStorage {
+    // State variable to store a number
+    uint public num;
+
+    // You need to send a transaction to write to a state variable.
+    function set(uint _num) public {
+        num = _num;
+    }
+
+    // You can read from a state variable without sending a transaction.
+    function get() public view returns (uint) {
+        return num;
+    }
+}
